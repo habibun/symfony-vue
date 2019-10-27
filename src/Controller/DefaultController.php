f5cfb0fdbd97b3b4448eebcbc64231cb80cf1d75
Habibun Noby <habibun.noby@gmail.com>
2019-10-27 23:08:24 +0600

    Add vue routing

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController']);
+    }
+}
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class IndexController extends AbstractController
+{
+    /**
+     * @Route("/{vueRouting}", name="index")
+     */
+    public function indexAction(): Response
+    {
+        return $this->render('base.html.twig', []);
+    }
+}
